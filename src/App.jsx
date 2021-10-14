01f0dfe116b2e309a12b8be63d144bd7b61dfe3f
Gabriel <gabrielalejandro.br99@gmail.com>
2021-10-14 01:36:43 -0500

    udpate
    fixed responsiveness in desktop mode

--- src/App.jsx
+++ src/App.jsx
@@ -14,33 +14,33 @@ function App() {
 
   return (
     <div className="App">
-      <NavigationBar />
+        <NavigationBar />
 
-      <section className="section mainContent">
-        <h1>I design, code and <br /> launch Websites and <br /> apps, amongst other <br />things.</h1>
-        {/* 🎨 💻 🚀*/}
-        <Button message="Get in touch!" icon="envelope" />
-      </section>
+        <section className="section mainContent Tilt-inner" id="home">
+          <h1>I design, code and <br /> launch Websites and <br /> apps, amongst other <br />things.</h1>
+          {/* 🎨 💻 🚀*/}
+          <Button message="Get in touch!" icon="envelope" />
+        </section>
 
-      <section className="section aboutContent">        
-        <ABioCard
-          head="Nice to meet you!"
-          pic="src\resources\profile pic 2.jpg"
-          bio="My name is Gabriel, im a Software engineer student from México and i do Web development on my free time, i’m also a self taught UI and Web designer."
-          btnTxt="More"
-          btnIcon="plus"
-        />
-      </section>
+        <section className="section aboutContent" id="about">
+          <ABioCard
+            head="Nice to meet you!"
+            pic="src\resources\profile pic 2.jpg"
+            bio="My name is Gabriel, im a Software engineer student from México and i do Web development on my free time, i’m also a self taught UI and Web designer."
+            btnTxt="More"
+            btnIcon="plus"
+          />
+        </section>
 
-      <section className="section aboutContent">        
-        <Button message="Carousel" />
-      </section>
+        <section className="section aboutContent" id="work">
+          <Button message="Carousel" />
+        </section>
 
-      <section className="section aboutContent">        
-        <Button message="Full screen footer" />
-      </section>
+        <section className="section aboutContent" id="contact">
+          <Button message="Full screen footer" />
+        </section>
 
-      <Background />
+        <Background />
     </div>
   )
 }
--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const NavigationBar = () => {
+
+    
+
     return (
         <nav>
             <ul className="navigationBar">
-                <li><a href="#" className="navigationItemActive">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">My Work</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="#home" className="navigationItemActive">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#work">My Work</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     )
