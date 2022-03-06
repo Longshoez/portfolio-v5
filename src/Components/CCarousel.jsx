e7317bd2850dd1c01eb237e35bc47a387069ae88
Gabriel <gabrielalejandro.br99@gmail.com>
2022-03-06 02:32:49 -0600

    animating  carousel

--- src/Components/CCarousel.jsx
+++ src/Components/CCarousel.jsx
@@ -1,31 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from './Button'
 import './sCarousel.sass'
-import 'animate.css';
-
+import './animating.css';
 
 const CCarousel = ({items, time = 1000}) => {
   
   const [count, setCount] = useState(0)
+  const [timer, setTimer] = useState(time)
+  const [animating, setAnimating] = useState(false)
 
   setTimeout(() => {
-    count <= items.length -2 ? setCount(count +1) : setCount(0) //why it has to be -2?     
+    count <= items.length -2 ? (setCount(count +1), setAnimating(false)) : (setCount(0), setAnimating(true)) //why it has to be -2?      
   }, time);
+  
+  //make a timer and set animating to true if time == 0 then to false quickly so it launches only one time
+  
+
+  //i need the variable animating to be changing, true when the data changes and false before it leaves, so before the time goes to 0
 
   return (
       <div className="carousel">
           <div className="data">
-          <h1>{items[count].name}</h1>
-          <p>{items[count].dscription}</p>
-              <div className="c-buttons">
-                  <Button message={"more"} icon={"plus"} size="small"/>
-                  <Button state={items[count].status ? "" : "deactivated"} message={"working demo"} icon={"open"} size="small" link={items[count].status && items[count].link}/>
-                  <Button message={"previous"} icon={"chevron-left"} lean={"left"} size="small" />
-                  <Button message={"next proyect"} icon={"chevron-right"} size="small"/>
-              </div>          
+          <h1 className={!animating ? "slide" : ''}>{items[count].name}</h1>
+          <p className={!animating ? "slide" : ''}>{items[count].dscription}</p>
+          <div className="c-buttons">
+              <Button message={"more"} icon={"plus"} size="small"/>
+              <Button state={items[count].status ? "" : "deactivated"} message={"working demo"} icon={"open"} size="small" link={items[count].status && items[count].link}/>
+              <Button message={"previous"} icon={"chevron-left"} lean={"left"} size="small" />
+              <Button message={"next project"} icon={"chevron-right"} size="small"/>
+          </div>          
           </div>
-          <div className='image-container'>
-            <p>{items[count].id} / {items.length}</p>
+          <div className={!animating ? "slide image-container" : 'image-container'}>           <p>{items[count].id} / {items.length}</p>
             <img src={items[count].image} alt="" />
           </div>
       </div>
